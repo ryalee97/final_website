2103565a621dd5483a15c4e3a679d68c243353a4
ryalee97 <ryalee97@gmail.com>
2018-08-30 03:31:32 -0400

    Added resume

--- src/components/intro.js
+++ src/components/intro.js
@@ -5,8 +5,31 @@ import resume_icon from "../images/resume_icon_new.png"
 import linkedin_icon from "../images/linkedin_icon.png"
 import resume from "../documents/RyanLeeResume.pdf"
 
+function openResumeModal() {
+  var modal = document.getElementById('resume_modal')
+  modal.style.display = "block"
+  var body=document.getElementsByTagName('body')[0]
+  body.style.overflow="hidden"
+}
+
+function closeResumeModal() {
+  var modal = document.getElementById('resume_modal')
+  modal.style.display = "none"
+  var body=document.getElementsByTagName('body')[0]
+  body.style.overflow="auto"
+}
+
 const Intro = () => (
   <div className ="intro">
+  	<div id="resume_modal" className="intro_modal">
+    	<div className = "intro_modal-content">
+    		<span className="close" onClick={() => closeResumeModal()}>&times;</span>
+    		<div className="resume">
+	    		<embed src={resume} width="800" height="1000" 
+	 			type='application/pdf'/>
+	 		</div>
+    	</div>
+    </div>
   	<div className ="intro_cont">
 		<Link to="#"><img src={profile}/></Link>
 		<span>
@@ -16,7 +39,7 @@ const Intro = () => (
 		    	Senior at Carnegie Mellon University studying<b> Electrical and Computer Engineering</b>.
 		    	Aside from programming, I love to travel the world to<b> serve</b> communities in need.
 			</h2>
-			<Link to={resume}><img src={resume_icon}/></Link>
+			<a href="#" onClick={() => openResumeModal()}><img src={resume_icon}/></a>
 			<a href="https://www.linkedin.com/in/ryalee97"><img src={linkedin_icon}/></a>
 			<h1></h1>
 		</span>
